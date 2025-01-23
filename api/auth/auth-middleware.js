c14e8435371bedb74c3c281f7266c3d1c8bc8063
Conner Krenzer <krenzer84@gmail.com>
2025-01-23 11:44:24 -0500

    finnished

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,14 +1,34 @@
-// AUTHENTICATION
-const restricted = (req, res, next) => {
-  next()
-}
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../../config");
 
-// AUTHORIZATION
-const checkRole = (req, res, next) => {
-  next()
-}
+// AUTHENTICATION -- identity, who you are
+const restricted = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, JWT_SECRET, (error, decoded) => {
+      if (error) {
+        next({ status: 401, message: `token bad: ${error.message}` });
+      } else {
+        req.decodedJwt = decoded;
+        console.log(req.decodedJwt);
+        next();
+      }
+    });
+  } else {
+    next({ status: 401, message: "wat? no token?" });
+  }
+};
+
+// AUTHORIZATION -- permissions on what the user has
+const checkRole = (role) => (req, res, next) => {
+  if (req.decodedJwt && req.decodedJwt.role === role) {
+    next();
+  } else {
+    next({ status: 403, message: "you have no power here!" });
+  }
+};
 
 module.exports = {
   restricted,
   checkRole,
-}
+};
